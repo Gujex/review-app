c272f3ee5890a541472dbfc124dba64548d41016
Gujex <txere@bk.ru>
2021-11-13 22:14:44 +0400

    Updates

---
+++ precache-manifest.9782418c1edc752a4acf804ffa42ed7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d258756268fa644ba7546f6f5aad4280",
+    "url": "/review-app/index.html"
+  },
+  {
+    "revision": "041ee737a8f6dcd77cd5",
+    "url": "/review-app/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/review-app/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/review-app/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "041ee737a8f6dcd77cd5",
+    "url": "/review-app/static/js/main.b75897ac.chunk.js"
+  },
+  {
+    "revision": "7502e036a60f526a42d3",
+    "url": "/review-app/static/js/runtime-main.c30795a3.js"
+  }
+]);
